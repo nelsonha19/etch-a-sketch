5e119b996cc4f59d5addb862c3ff52a4e1a13e63
Nelson Ha <nelsonha19@gmail.com>
2020-01-31 07:28:32 +1100

    style css, rainbow and write clear function

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
 const container = document.querySelector('.container');
 
 
+const rainbow = document.querySelector('.rainbow');
+const toggle = document.querySelector('.toggle');
+let on = false;
+
+rainbow.addEventListener('click', ()=> {
+    if(toggle.textContent == 'OFF'){
+        on = true;
+        if(text.value == '') {
+            drawDefaultGrid();
+        } else {
+            createGrid(text.value);
+        }
+        toggle.textContent = 'ON';
+    } else {
+        on = false;
+        if(text.value == '') {
+            drawDefaultGrid();
+        } else {
+            createGrid(text.value);
+        }
+        toggle.textContent = 'OFF';
+    }
+});
+
+let rainbowFunction = function(grid) {
+
+    grid.addEventListener('mouseover', () => {
+        let randomR = Math.floor(Math.random() * 256);
+        let randomG = Math.floor(Math.random() * 256);
+        let randomB = Math.floor(Math.random() * 256);
+
+        grid.style.backgroundColor = `rgb(${randomR},${randomG},${randomB})`;
+        grid.style.border = '1px solid rgba(0,0,0,0.5);';
+    });
+};
+
 /* DRAW DEFAULT GRID AT START*/
 let drawDefaultGrid = function() { 
-    const DEFAULT_NO_GRID = 8;
+    const DEFAULT_NO_GRID = 16;
     let totalNumberOfGrids = numberOfGrids(DEFAULT_NO_GRID);
     container.innerHTML = "";
 
@@ -19,9 +55,13 @@ let drawDefaultGrid = function() {
 
         container.appendChild(grid);
 
-        defaultHover(grid);
+        if(on) {
+            console.log('works');
 
-
+            rainbowFunction(grid);
+        } else {
+            defaultHover(grid);
+        }
     }
 }
 
@@ -43,30 +83,36 @@ let createGrid = function(number) {
 
         container.appendChild(grid);
         
-
-        defaultHover(grid);
-
+        if(on) {
+            rainbowFunction(grid);
+        } else {
+            defaultHover(grid);
+        }
 
     }
 }
 
+
+// PAINTING
+const red = 'red';
+
 let defaultHover = function (grid) {
     grid.addEventListener('mouseover', () => {
-        grid.style.backgroundColor = 'red';
-
+        grid.style.backgroundColor = red;
+        grid.style.border = '1px solid rgba(0,0,0,0.5);';
     });
-
 };
 
 const button = document.querySelector('.submit');
 const text = document.querySelector('.text');
 
+
 button.addEventListener('click', () => {
     let number = text.value;
-    if(number>0 && number <=32) {
+    if(number>0 && number <=64) {
         createGrid(number);
     } else {
-        alert("Please enter a number between 0 and 32");
+        alert("Please enter a number between 0 and 64");
         text.value = "";
     }
 
@@ -77,3 +123,14 @@ button.addEventListener('click', () => {
 numberOfGrids = (n) => n * n;
 
 drawDefaultGrid();
+
+// CLEAR BUTTON FUNCTION
+const clear = document.querySelector('.clear');
+clear.addEventListener('click', () => {
+    if(text.value == '') {
+        drawDefaultGrid();
+    } else {
+        createGrid(text.value);
+    }
+});
+
